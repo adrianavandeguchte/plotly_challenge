960a9d72025d27e00f4d73f593fb0a12032d2798
adrianavandeguchte <61213204+adrianavandeguchte@users.noreply.github.com>
2020-06-16 07:53:04 -0400

    Update app.js
    added bubble chart and altered code to allow for selection of other IDs

--- static/js/app.js
+++ static/js/app.js
@@ -16,11 +16,15 @@ d3.json(source).then(function(data) {
 });
 
 
+
 // Display the default plot
-function init() {
+function buildPlots() {
   d3.json(source).then(function(data) {
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var inputValue = dropdownMenu.property("value");
     // filter json data to only the sample records matching input ID
-    var sampleData = data.samples.filter(d => d.id === "940");
+    var sampleData = data.samples.filter(d => d.id === inputValue);
     // combine all otu info for sample into single array
     var subSelection = [];
       for (var j = 0; j < sampleData[0].otu_ids.length; j++)
@@ -33,23 +37,23 @@ function init() {
       reversed.map(row => row.otu_ids).forEach((i) => {
         labels.push("OTU " + i)
       });
-    var trace1 = [{
+    var barTrace = [{
       x: reversed.map(row => row.sample_values),
       y: labels,
       text: reversed.map(row => row.otu_labels),
       type: "bar",
       orientation: "h"
     }];
-
-    var layout = {
-      title: "Dominant OTUs in Individual"
+    var title = ("Dominant OTUs for ID " + inputValue)
+    var barLayout = {
+      title: title
     };
     // filter json data to only the metadata matching input ID
-    var metaData = data.metadata.filter(d => d.id === 940);
+    var metaData = data.metadata.filter(d => d.id === inputValue);
     console.log(metaData[0]);
     var metaTable = document.getElementById("sample-metadata");
-
-    // loop to add sample ids as dropdown options
+    metaTable.innerHTML = "";
+    // loop to add metadata table
     for (let [key, value] of Object.entries(metaData[0])) {
       var li = document.createElement('li');
       li.appendChild(document.createTextNode(`${key}: ${value}`));
@@ -57,72 +61,89 @@ function init() {
       console.log(`${key}: ${value}`);
     }
 
+    // build of bubble plot
+    var bubbleTrace = [{
+      x: subSelection.map(row => row.otu_ids),
+      y: subSelection.map(row => row.sample_values),
+      mode: 'markers',
+      text: subSelection.map(row => row.otu_labels),
+      marker: {
+        size: subSelection.map(row => row.sample_values),
+        color: subSelection.map(row => row.otu_ids)
+      }
+    }];
+
+    var bubbleLayout = {
+      title: 'OTU Distribution',
+      showlegend: false,
+    };
 
 
-
-    Plotly.newPlot("bar", trace1, layout);
+    Plotly.newPlot("bar", barTrace, barLayout);
+    Plotly.newPlot('bubble', bubbleTrace, bubbleLayout);
   });
 };
 
 // On change to the DOM, call getData()
-d3.selectAll("#selDataset").on("change", getData);
+d3.selectAll("#selDataset").on("change", buildPlots);
 
-// Function called by DOM changes
-function getData() {
-  var dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  var inputValue = dropdownMenu.property("value");
-  // Fetch the JSON data and console log it
-  d3.json(source).then(function(data) {
-    console.log(data)
-  // filter json data to only the sample records matching input ID
-  var sampleData = data.samples.filter(d => d.id === inputValue);
-    // sort and slice the filtered data to get top 10 OTUs
-    var subSelection = [];
-      for (var j = 0; j < sampleData[0].otu_ids.length; j++)
-          subSelection.push({'otu_ids': sampleData[0].otu_ids[j], 'otu_labels': sampleData[0].otu_labels[j], 'sample_values': sampleData[0].sample_values[j]});
-      // sort and slice the filtered data to get top 10 OTUs
-      console.log(subSelection);
-      var sortedData = subSelection.sort((a,b)=>b.sample_values-a.sample_values);
-      var top10 = sortedData.slice(0,10);
-      var reversed = top10.reverse();
+// // Function called by DOM changes
+// function getData() {
+//   var dropdownMenu = d3.select("#selDataset");
+//   // Assign the value of the dropdown menu option to a variable
+//   var inputValue = dropdownMenu.property("value");
+//   console.log(inputValue);
+//   // Fetch the JSON data and console log it
+//   d3.json(source).then(function(data) {
+//     console.log(data)
+//   // filter json data to only the sample records matching input ID
+//   var sampleData = data.samples.filter(d => d.id === inputValue);
+//     // sort and slice the filtered data to get top 10 OTUs
+//     var subSelection = [];
+//       for (var j = 0; j < sampleData[0].otu_ids.length; j++)
+//           subSelection.push({'otu_ids': sampleData[0].otu_ids[j], 'otu_labels': sampleData[0].otu_labels[j], 'sample_values': sampleData[0].sample_values[j]});
+//       // sort and slice the filtered data to get top 10 OTUs
+//       console.log(subSelection);
+//       var sortedData = subSelection.sort((a,b)=>b.sample_values-a.sample_values);
+//       var top10 = sortedData.slice(0,10);
+//       var reversed = top10.reverse();
+//
+//   // filter json data to only the metadata matching input ID
+//   var metaData = data.metadata.filter(d => d.id === inputValue);
+//   // filter json data to only the metadata matching input ID
+//     var ethnicity = data.metadata.ethnicity;
+//     var gender = data.metadata.gender;
+//     var age = data.metadata.age;
+//     var location = data.metadata.location;
+//     var bbtype = data.metadata.bbtype;
+//     var wfreq = data.metadata.wfreq;
+//   updatePlotly(reversed);
+//   });
+// };
+//
+// function updatePlotly(newdata) {
+//   var newLabels = [];
+//   console.log(newdata);
+//   newdata.map(row => row.otu_ids).forEach((i) => {
+//     newLabels.push("OTU " + i)
+//   });
+//   console.log(newLabels)
+//   var update = {
+//       x: newdata.map(row => row.sample_values),
+//       y: newLabels,
+//       text: newdata.map(row => row.otu_labels),
+//       type: "bar",
+//       orientation: "h"
+//   }
+//   Plotly.restyle("bar", update);
+// };
+//
+//
+// // Promise Pending
+// const dataPromise = d3.json(source);
+// console.log("Data Promise: ", dataPromise);
 
-  // filter json data to only the metadata matching input ID
-  var metaData = data.metadata.filter(d => d.id === inputValue);
-  // filter json data to only the metadata matching input ID
-    var ethnicity = data.metadata.ethnicity;
-    var gender = data.metadata.gender;
-    var age = data.metadata.age;
-    var location = data.metadata.location;
-    var bbtype = data.metadata.bbtype;
-    var wfreq = data.metadata.wfreq;
-  updatePlotly(reversed);
-  });
-};
-
-function updatePlotly(newdata) {
-  var newLabels = [];
-  console.log(newdata);
-  newdata.map(row => row.otu_ids).forEach((i) => {
-    newLabels.push("OTU " + i)
-  });
-  console.log(newLabels)
-  var update = {
-      x: newdata.map(row => row.sample_values),
-      y: newLabels,
-      text: newdata.map(row => row.otu_labels),
-      type: "bar",
-      orientation: "h"
-  }
-  Plotly.restyle("bar", update);
-};
-
-
-// Promise Pending
-const dataPromise = d3.json(source);
-console.log("Data Promise: ", dataPromise);
-
-init();
+buildPlots();
 
 
 //
